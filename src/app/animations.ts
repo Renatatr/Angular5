d43f22428be4846e0342dcd948c278756f224188
Renatatr <78363362+Renatatr@users.noreply.github.com>
2023-11-07 11:55:41 -0300

    Animação com query

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { animate, group, keyframes, state, style, transition, trigger } from "@angular/animations";
+import { animate, group, keyframes, query, state, style, transition, trigger } from "@angular/animations";
 
 export const highlightedStateTrigger = trigger('highlightedState', [
     state('default', style({
@@ -76,30 +76,35 @@ export const filterTrigger = trigger('filterAnimation', [
 
 export const formButtonTrigger = trigger('formButton', [
     transition('invalid => valid', [
-        group([
+        query('#botao-salvar', [
+            group([
+                animate('200ms', style({
+                    backgroundColor: '#63B77C'
+                })),
+                animate('100ms', style({
+                    transform: 'scale(1.1)'
+                }))
+            ]),
             animate('200ms', style({
-                backgroundColor: '#63B77C'
-            })),
-            animate('100ms', style({
-                transform: 'scale(1.1)'
+                transform: 'scale(1)'
             }))
         ]),
-        animate('200ms', style({
-            transform: 'scale(1)'
-        }))
     ]),
     transition('valid => invalid', [
-        group([
+        query('#botao-salvar', [
+            group([
+                animate('200ms', style({
+                    backgroundColor: '#6C757D'
+                })),
+                animate('100ms', style({
+                    transform: 'scale(1.1)'
+                }))
+            ]),
             animate('200ms', style({
-                backgroundColor: '#6C757D'
-            })),
-            animate('100ms', style({
-                transform: 'scale(1.1)'
+                transform: 'scale(1)'
             }))
-        ]),
-        animate('200ms', style({
-            transform: 'scale(1)'
-        }))
+        ])
+
     ])
 ])
 
